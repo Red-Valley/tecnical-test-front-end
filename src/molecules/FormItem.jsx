eeb144045586e5e0178f7ba6dfe9ee80c255c71a
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-12 10:55:23 -0500

    form item changes

---
+++ src/molecules/FormItem.jsx
@@ -0,0 +1,10 @@
+export const FormItem = ({ hideLabel, children, htmlFor, className }) => (
+  <div class={`mb-3 ${className}`}>
+    {!hideLabel ? (
+      <label htmlFor={htmlFor} className="form-label">
+        Default file input example
+      </label>
+    ) : null}
+    {children}
+  </div>
+);
