54994f242696040b41b46a95375dfcc10f65241f
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-13 09:27:14 -0500

    there should be a padding on bottom

--- src/organisms/CardsGrid.jsx
+++ src/organisms/CardsGrid.jsx
@@ -1,11 +1,12 @@
 import { CharacterCard } from "../molecules/CharacterCard";
+import "../styles/scss/organisms/cards-grid.scss";
 
 export const CardsGrid = ({ grid = [] }) => (
-  <section className="container">
+  <section className="container" id="main">
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       {grid.map(({ name, image, id }) => (
         <div className="col" key={id}>
-          <CharacterCard name={name} id={id} imageSrc={image} />
+          <CharacterCard name={name} id={id} imageSrc={image} showLink />
         </div>
       ))}
     </div>
