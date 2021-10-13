18d3b2e1062371e5ac0441b421524158fc77eb38
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-13 09:26:55 -0500

    using loader on home template

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,6 +1,7 @@
 import { Header } from "../organisms/Header";
 import { CardsGrid } from "../organisms/CardsGrid";
 import { useGetCharactersListQuery } from "../features/characters/characters-slice";
+import { CubeGrid } from "../atoms/CubeGrid";
 
 export const Home = () => {
   const { data = [], isFetching } = useGetCharactersListQuery();
@@ -8,7 +9,14 @@ export const Home = () => {
   return (
     <div className="container">
       <Header />
-      {isFetching ? "...Loading" : ""}
+      {isFetching ? (
+        <div
+          style={{ margin: "5rem 0 1rem 0", width: "100%" }}
+          className="d-flex justify-content-center"
+        >
+          <CubeGrid />
+        </div>
+      ) : null}
       <CardsGrid grid={data} />
     </div>
   );
