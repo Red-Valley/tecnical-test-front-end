02d431aa4980c4f48067bcc2b4be273ffb0f3c72
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-15 15:18:04 -0500

    updating home template accordingly

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,44 +1,43 @@
+import { useEffect, useCallback } from "react";
 import { Header } from "../organisms/Header";
 import { CardsGrid } from "../organisms/CardsGrid";
-import { useGetCharacterByNameQuery } from "../features/characters/characters-slice";
 import { CubeGrid } from "../atoms/CubeGrid";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Pagination } from "../molecules/Pagination";
+import {
+  changePage,
+  changeTotalCount,
+} from "../features/characters/pagination-slice";
+import {
+  selectLoading,
+  fetchCharacters,
+  selectDataInfoCount,
+} from "../features/characters/api-slice";
+import { selectFilter } from "../features/characters/search-slice";
+
+import "../styles/scss/templates/home.scss";
 
 export const Home = () => {
-  const [filter, setFilter] = useState("");
-  const [currentData, setCurrentData] = useState([]);
-  const { data: mountedData, isFetching } = useGetCharacterByNameQuery(filter);
+  const dispatch = useDispatch();
+  const { search: filter } = useSelector(selectFilter);
+  const isLoading = useSelector(selectLoading);
+  const { currentPage, totalCount } = useSelector((state) => state.pagination);
+  const count = useSelector(selectDataInfoCount);
 
-  const filteredCharacter = useSelector(
-    (state) => state.characters.queries[`getCharacterByName("${filter}")`]
-  );
-
-  function handleSearchChange(s) {
-    setFilter(s);
-  }
-
-  // Load characters data base on filter changes
+  // On pagination or filter changes
   useEffect(() => {
-    if (filteredCharacter) {
-      setCurrentData(filteredCharacter.data);
-    }
-  }, [filter, filteredCharacter]);
+    dispatch(fetchCharacters({ page: currentPage, name: filter }));
+  }, [currentPage, filter, dispatch]);
 
-  // Load all characters data on mounted
+  // Pagination items total change
   useEffect(() => {
-    let mounted = true;
-    if (mounted && mountedData) {
-      setCurrentData(mountedData.results);
-    }
-    return () => (mounted = false);
-  }, [mountedData]);
+    dispatch(changeTotalCount(count));
+  }, [count, dispatch]);
 
   return (
-    <div className="container">
-      <Header handleSearch={handleSearchChange} />
-      {isFetching ? (
+    <div className="container" id="home">
+      <Header />
+      {isLoading ? (
         <div
           style={{ margin: "5rem 0 1rem 0", width: "100%" }}
           className="d-flex justify-content-center"
@@ -46,7 +45,15 @@ export const Home = () => {
           <CubeGrid />
         </div>
       ) : null}
-      <CardsGrid grid={currentData} />
+      <CardsGrid />
+      <div className="d-flex justify-content-center align-items-center">
+        <Pagination
+          currentPage={currentPage}
+          totalCount={totalCount}
+          pageSize={20}
+          onPageChange={(page) => dispatch(changePage(page))}
+        />
+      </div>
     </div>
   );
 };
