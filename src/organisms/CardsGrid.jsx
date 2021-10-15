5a96db37b25f71d6781a7f04f103fab088480461
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-15 15:17:26 -0500

    reading data directly from store state

--- src/organisms/CardsGrid.jsx
+++ src/organisms/CardsGrid.jsx
@@ -1,14 +1,19 @@
+import { useSelector } from "react-redux";
+import { selectData } from "../features/characters/api-slice";
 import { CharacterCard } from "../molecules/CharacterCard";
 import "../styles/scss/organisms/cards-grid.scss";
 
-export const CardsGrid = ({ grid = [] }) => (
-  <section className="container" id="main">
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      {grid.map(({ name, image, id }) => (
-        <div className="col" key={id}>
-          <CharacterCard name={name} id={id} imageSrc={image} showLink />
-        </div>
-      ))}
-    </div>
-  </section>
-);
+export const CardsGrid = () => {
+  const data = useSelector(selectData);
+  return (
+    <section className="container" id="main">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        {data.map(({ name, image, id }) => (
+          <div className="col" key={id}>
+            <CharacterCard name={name} id={id} imageSrc={image} showLink />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
