2972132fdcd9d12a7930da5b3031a574b7ffd6cd
Manuel Blanco <manuel.22.lamas@gmail.com>
2021-10-15 15:17:37 -0500

    making filter changes directly to the store

--- src/organisms/Header.jsx
+++ src/organisms/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { Search } from "../molecules/Search";
 import { setFilter } from "../features/characters/search-slice";
+import { useDispatch } from "react-redux";
 
 export const Header = () => {
+  const dispatch = useDispatch();
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
@@ -10,7 +12,7 @@ export const Header = () => {
           <h1 className="h4">Brand</h1>
         </Link>
         <div style={{ marginTop: "1rem" }} id="search-wrapper">
-          <Search onSearch={(s) => setFilter(s)} />
+          <Search onSearch={(s) => dispatch(setFilter(s))} />
         </div>
       </div>
     </nav>
