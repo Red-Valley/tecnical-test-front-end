fe1b3a75a28c5769e1908750cb63bdfcc16d8176
samumirandam <samuelchampi@gmail.com>
2022-01-22 17:29:55 -0500

    Se inicia proyecto

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <section className="Home">
+      <h1>tecnical-test-front-end</h1>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <section>
+      No encontrado, error 404.
+    </section>
+  );
+}
+
+export default NotFound;
