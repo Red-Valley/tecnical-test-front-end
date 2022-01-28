824d23701c2364aa15d5959dbb90bb61deafcd25
JoseMejia <josemejia11@hotmail.es>
2022-01-28 11:24:17 -0500

    feat: edited test

--- src/components/molecules/card-detail/card-detail.test.js
+++ src/components/molecules/card-detail/card-detail.test.js
@@ -3,23 +3,28 @@ import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import CardMolecule from './index';
 
-test('renders content correctly', () => {
-    const character = {
-            id: 1,
-            name: 'character test',
-            image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-            location: {
-                name: 'Earth'
-            },
-            status: 'Alive',
-            origin: {
-                name: 'Earth'
-            },
-            gender: 'Male',
-            species: 'Human',
-            type: "type"
-        }
+const character = {
+    id: 1,
+    name: 'character test',
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    location: {
+        name: 'Earth'
+    },
+    status: 'Alive',
+    origin: {
+        name: 'Earth'
+    },
+    gender: 'Male',
+    species: 'Human',
+    type: "type"
+}
 
+test('content render', () => {
+    const view = render(<CardMolecule character={character} />);
+    expect(view).toBeDefined();
+});
+
+test('content render correctly', () => {
     const view = render(<CardMolecule character={character} />);
     expect(view.container).toHaveTextContent(character.name);
 });
